7b01319d684d2ac65e955ab982de2a86d4a01f82
Arthur Martins de Souza Barreto <82684357+Arthur-Barreto@users.noreply.github.com>
2022-10-16 20:27:40 -0300

    my bls using linear regression to find the transit

--- Lightkurve/myBls.py
+++ Lightkurve/myBls.py
@@ -19,8 +19,15 @@ def deltaY(vetor):
         lis.append(delta)
     return lis
 
+def linearRegressionCoeficient(dataX,dataY):
+    mediaX = np.mean(dataX)
+    mediaY = np.mean(dataY)
+    SSxx = np.sum(np.fromiter(((mediaX-x)**2 for x in  dataX),dtype=float))
+    SSxy = np.sum(np.fromiter(((mediaX-x)*(mediaY-y) for x,y in zip(dataX,dataY)),dtype=float))
+    return SSxy/SSxx
+
 # função que calcula o BLS, dado o tempo e o fluxo
-def bls(time,fluxo,delta):
+def bls(time,fluxo,error):
     # time é o eixo x
     # fluxo é o eixo y
     # na lisPeiod vamos guardar os possiveis periodos, no final retornar o maior valor
@@ -28,24 +35,35 @@ def bls(time,fluxo,delta):
     # time e fluxo devem ter o mesmo tamanho
     if len(time) != len(fluxo):
         print("Erro: time e fluxo devem ter o mesmo tamanho")
-        return 0
+        return -1
     # vamos verificar se a curva está normalizada, se não iremos normalizar
     if np.mean(fluxo) != 0 or np.std(fluxo) != 1:
-        # normalizando
         fluxo = normaliza(fluxo)
-    # o periodo é a direferença entre dois pontos consecutivos do vetor time
-    # a intenção é calcular todos os periodos e retornar o maior deles
-    lisDelta = deltaY(fluxo)
-    # vamos verificar os dados, se o delta for proximo, o periodo será o indice dentro deste intervalo
-    # se a diferença entre os pontos de lisDelta for menor que delta, continuamos a analise
+    # agora vamos iterar sobre a lista fluxo, calcular a reg linear
+    # caso modulo do coeficiente suba muito, temos uma região de transito
+
+    oldCoef = None
+    coef = None
     start = 0
-    for i in range(len(lisDelta)):
-        # se a diferença for menor que delta, continuamos
-        if lisDelta[i] > delta:
-            # a diferença entre os dois pontos não representa parte do poço
-            # vamos calcular o periodo
-            periodo = time[i+1] - time[start]
+
+    for i in range(1,len(fluxo)):
+        smallList = fluxo[:i]
+        coef = linearRegressionCoeficient(time[:i],smallList)
+        # ao calcular o coef da regresão linear, saberemos o comportamento da curva
+        # se ela inclinar muito, temos uma região de transito
+
+        # vamos testar para modulo maior que 0.5
+        if abs(coef) > 0.5:
+            oldCoef = coef
+            start = i
+
+        # se a diferença entre o coefiente antigo e o novo for maior que o erro permitido
+        # temos o fim do transito, e o indice atual é o novo start 
+        if abs(oldCoef-coef) > error:
+            # temos o indicativo do fim do transito, no caso será o i-1
+            end = i-1
+            periodo = time[end] - time[start]
             lisPeriod.append(periodo)
-            # atualizando o start
-            start = i    
+            start = i
+
     return np.max(lisPeriod)
